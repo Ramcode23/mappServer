5f30b636bb577b7c3f539a39d5d659a7ce9decf7
Raphy Mejia <83966299+Ramcode23@users.noreply.github.com>
2022-02-15 13:29:47 -0800

    Update Server.js

--- Server.js
+++ Server.js
@@ -6,7 +6,7 @@ const http= require('http');
 const server=http.createServer(app);
 app.use(cors());
 
-const PORT=5000;
+const PORT=process.env.PORT || 3000;
  const io = require('socket.io')(server,{
 cors: {
     origin: ['http://localhost:4200']
